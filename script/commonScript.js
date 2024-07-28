4f7e65d2354c1602d0fc35f825f29ea8277a00c8
Ani Bozhkova <ani2003ab@gmail.com>
2024-07-28 20:32:38 +0300

    Navigation desigh change and carousel added to the main page.

--- script/commonScript.js
+++ script/commonScript.js
@@ -1,39 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.getElementById('burger-menu');
+    const overlay = document.getElementById('overlay');
     document.getElementById('burger-menu-icon').addEventListener('click', function () {
-        document.getElementById('burger-menu').classList.toggle('active');
-        document.getElementById('overlay').classList.toggle('active');
+        burgerMenu.classList.toggle('active');
+        overlay.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 900) { 
+            burgerMenu.classList.remove('active');
+            overlay.classList.remove('active');
+        }
     });
 });
 
-
-
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel__slide');
-const totalSlides = slides.length;
-const slideInterval = 5000; // 5 seconds
-
-function showSlide(index) {
-    if (index >= totalSlides) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = totalSlides - 1;
-    } else {
-        currentSlide = index;
-    }
-    const newTransform = `translateX(-${currentSlide * 100}%)`;
-    document.querySelector('.carousel__viewport').style.transform = newTransform;
-}
-
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
-
-// Initialize the carousel to show the first slide
-showSlide(currentSlide);
-
-// Set interval to automatically change slides
-setInterval(nextSlide, slideInterval);
--- script/homeScript.js
+++ script/homeScript.js
@@ -114,3 +114,34 @@ function getUserData(loggedInUserEmail) {
 function getLoggedInUserEmail() {
     return localStorage.getItem('loggedInUserEmail');
 }
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel__slide');
+const totalSlides = slides.length;
+const slideInterval = 6000; // 6 seconds
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    const newTransform = `translateX(-${currentSlide * 100}%)`;
+    document.querySelector('.carousel__viewport').style.transform = newTransform;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initialize the carousel to show the first slide
+showSlide(currentSlide);
+
+// Set interval to automatically change slides
+setInterval(nextSlide, slideInterval);
