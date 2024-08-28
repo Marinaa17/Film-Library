4693d6f37304823aa6f14ac4f6214b293bae41a7
Marina <marinakrusteva03@gmail.com>
2024-08-28 18:08:09 +0300

    Added username input in the sign up form. Improved sign up validation and the storage of users. Now all registered users are stored in single array in the local storage under the key users

--- script/signUpScript.js
+++ script/signUpScript.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('signUp');
     btn.addEventListener("click", () => {
 
+        document.getElementById("usernameErrorMessage").innerText = "";
         document.getElementById("emailErrorMessage").innerText = "";
         document.getElementById("passwordErrorMessage").innerText = "";
         document.getElementById("confirmPasswordErrorMessage").innerText = "";
@@ -13,20 +14,36 @@ document.addEventListener('DOMContentLoaded', function () {
 function validateSignUp() {
 
     var passed = true;
+    var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
 
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var usernameAndPasswordRegex = /^[A-Za-z0-9]+$/;
+
+    if (username.length < 3) {
+        document.getElementById("usernameErrorMessage").innerText = "Username should be at least 3 characters.";
+        passed=false;
+    }
+    if (!usernameAndPasswordRegex.test(username)) { 
+        document.getElementById("usernameErrorMessage").innerText = "The username should contain only Latin letters or numbers.";
+        passed = false;
+    }
 
     if (!emailRegex.test(email)) {
         document.getElementById("emailErrorMessage").innerText = "Please enter a valid email address.";
         passed=false;
     }
 
+
     if (password.length < 6) {
         document.getElementById("passwordErrorMessage").innerText = "Password should be at least 6 characters.";
         passed=false;
+    }
+    if (!usernameAndPasswordRegex.test(password)) { 
+        document.getElementById("passwordErrorMessage").innerText = "The password should contain only Latin letters or numbers.";
+        passed = false;
     }
 
     if (password !== confirmPassword) {
@@ -37,19 +54,26 @@ function validateSignUp() {
     return passed;
 }
 
-function getUserData(email) {
-    const userDataString = localStorage.getItem(email);
+function registerUser(username, email, password) {
 
-    return userDataString ? JSON.parse(userDataString) : null;
-}
+    const usersString = localStorage.getItem("users");
+    const users = usersString ? JSON.parse(usersString) : [];
 
-function registerUser(email, password) {
-    if (getUserData(email)) {
+    const usernameExists = users.some(user => user.username === username);
+    const emailExist = users.some(user => user.email === email);
+
+    if (usernameExists) {
+        document.getElementById("signUpErrorMessage").innerText = "User with this username already exists.";
+        return;
+    }
+
+    if (emailExist) {
         document.getElementById("signUpErrorMessage").innerText = "User with this email already exists.";
         return;
     }
 
     const userData = {
+        username: username,
         email: email,
         password: password,
         favourites: [],
@@ -57,9 +81,9 @@ function registerUser(email, password) {
         addedMovies: []
     };
 
-    const userDataString = JSON.stringify(userData);
-    debugger;
-    localStorage.setItem(email, userDataString);
+
+    users.push(userData);
+    localStorage.setItem("users", JSON.stringify(users));
 
     window.location.href = "logIn.html";
 
@@ -68,9 +92,10 @@ function registerUser(email, password) {
 
 function validateAndRegister() {
     if (validateSignUp()) {
+        const username = document.getElementById("username").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
-        registerUser(email, password);
+        registerUser(username, email, password);
     }
 }
