7ad403eee6e6213e4b2159cd6e9d0065f0a289eb
Ani Bozhkova <ani2003ab@gmail.com>
2024-08-28 17:22:06 +0300

    Added logout confirmation.

--- script/commonScript.js
+++ script/commonScript.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burger-menu');
     const overlay = document.getElementById('overlay');
+    const logoutButton = document.getElementById('logout');
     document.getElementById('burger-menu-icon').addEventListener('click', function () {
         burgerMenu.classList.toggle('active');
         overlay.classList.toggle('active');
@@ -10,6 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
         if (window.innerWidth > 900) { 
             burgerMenu.classList.remove('active');
             overlay.classList.remove('active');
+        }
+    });
+
+    logoutButton.addEventListener('click', function (event) {
+        const userConfirmed = confirm('Are you sure you want to log out?');
+
+        if (!userConfirmed) {
+            event.preventDefault();
         }
     });
 });
